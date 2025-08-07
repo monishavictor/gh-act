638bc0045530c29a5a7291698b48845d66aeb9a1
Monisha Victor <monishavictor987@gmail.com>
2025-08-07 07:29:06 +0530

    check

---
+++ scripts/is_feature_app.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+printenv
+
+gh
+
+# echo "Check if PR \"$TRAVIS_BRANCH\" has a feature app label."
+
+# if ! command -v gh &> /dev/null
+# then
+#     echo "Github CLI was not found, installing..."
+
+#     curl -sSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+#     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+#     sudo apt-get update
+#     sudo apt-get install gh
+
+#     gh auth status
+# fi
+
+# existing_pull_requests=$(gh pr list -H $TRAVIS_BRANCH --repo $TRAVIS_REPO_SLUG --state open --limit 1 --label "Feature App")
+
+# if [[ -z "$existing_pull_requests" ]]; then
+#     echo "No pull requests found for branch $TRAVIS_BRANCH or release branch with the label Feature App attached."
+#     exit 1
+# else
+#     echo "Pull requests found for branch $TRAVIS_BRANCH with the label Feature App attached."
+#     exit 0
+# fi
